3754d3d91768b1da8b64d515d8cfdf7cbd0bb905
Carrington Jones <cjones6096@greenvilleschools.us>
2017-10-16 21:30:55 -0400

    GOTTA FIX SOME STUFF, MOSTLY WORKING

--- Rover.java
+++ Rover.java
@@ -5,92 +5,236 @@
  * @author Carrington Jones 
  * @version (9/21/17)
  */
-public class Rover
-{
+
+public class Rover {
     // fields
     private String name;
     private int x;
     private int y;
     private int dir; // 0=North, 1=East, 2=South, 3=West
     private int numPics; 
-    private int maxPics;
-    private double energy;
-    private boolean isAlive; 
-
-    // constructor(s)
-    public Rover(String name, int x, int y, int dir) {
+    //private int maxPics;
+    // private double energy;
+    private boolean isAlive = true;
+    private int startingx;
+    private int startingy;
+    /**
+     * Constuctor(s)
+     * 
+     * @param name Naming the rover
+     */
+    public Rover(String name) {
         this.name = name;
-        this.x = x;
-        this.y = y;
+        this.x = 0;
+        this.y = 0;
         this.dir = dir;
-        this.isAlive = true; 
-        this.energy = 60;
-        this.maxPics = 15;
-        this.numPics = 0; 
+        // this.energy = 60;
+        this.dir = 0;
+        this.startingx = x;
+        this.startingy = y; 
     }
     /**
-     * The name of the rover is changed
-     * @param Name the name of the rover 
+     * Is returning the direction of the rover
+     * Once the direction of the rover is given, it tells you where its moved
+     * @param dir Direction
+     * @param direction The direction the rover is moving in numbers
+     * @param north,east,south,and west Moved in which direction 
+     * @param null Value of zero, no variable 
      */
+    private String getDirectionName(int dir) {
+        if (isAlive) {
+            if (dir == 0) {
+                return "North";
+            } else if (dir == 1) {
+                return "North North";
+            } else if (dir == 2) {
+                return "North East";
+            } else if (dir == 3) {
+                return "North South";
+            } else if (dir == 4) {
+                return "North West";
+            } else if (dir == 5) {
+                return "East";
+            } else if (dir == 6) {
+                return "East North";
+            } else if (dir == 7) {
+                return "East South";
+            } else if (dir == 8) {
+                return "East west";
+            } else {
+                return null;
+            }
+        } else {
+            System.out.println(name + " is disabled until further notice!");
+            return null;
+        }
+    }          
+    /**
+    * The name of the rover is changed
+    * @param Name the name of the rover 
+    */
     public void setName(String name) {
         this.name = name; 
     }
+    /** 
+     * @param getName() Public string, that returns the rover name 
+     */
     public String getName() {
-        return this.name; 
+        return name;
+    } 
+    ///**
+    //* Where the rover's die
+    //* @param death All rovers die and become unable to function 
+    //*/
+    //private void die() { 
+    //   isAlive = false; 
+    //    System.out.println(name + " is dead.");
+    //} 
+    /**
+     * Calling rover String name this.x and this.y
+     * @param 
+     */
+    public void teleport(int x, int y) {
+        System.out.println(name + "just teleported to [" + x + "," + y + "].");
+        this.x = x;
+        this.y = y; 
     } 
     // methods - stuff the Rover can do
-    public void move(int w)
-    {
-        if (dir == 0)
-        {
-            y += w;
+    /** 
+     * Move functions
+     * @param d Distance the rover is moving
+     * @param dir Direction in which the rover is moving 
+     * y = private int y, moves in the y direction
+     * x = private int x, moves in the x direction 
+     * Moves in 8 directions 
+     */
+    public void move(int d) {
+        if (dir == 0) {
+            y += d;
+        } else if (dir == 1) {
+            x += d;
+            y += d;
+        } else if (dir == 2) {
+            x += d; 
+        } else if (dir == 3) {
+            x += d;
+            y -= d; 
+        } else if (dir == 4) {
+            y -= d;
+        } else if (dir == 5) {
+            x -= d;
+            y -= d;
+        } else if (dir == 6) {
+            x -= d;
+        } else if (dir == 7) {
+            x -= d;
+            y += d;
         }
-        else if (dir == 1)
-        {
-            x += w;
-        }
-        else if (dir == 2)
-        {
-            y -= w;
-        }
-        else 
-        {
-            x -= w;
-        }
-        
-        this.energy -= (1*w);
-        
-        
-        //System.out.println(name + "moved" + getDirectionName(dir) + " by " + w);
-        //adjustEnergy(w * -5); 
+        System.out.println(name + " moved " + d + " units " + getDirectionName(dir) + ".");
     }
-    
-    public void rotateLeft() 
-    {
+        // this.energy -= (1*d);
+        // System.out.println(name + "moved" + getDirectionName(dir) + " by " + d);
+        // adjustEnergy(d * -5);
+     /**
+      * rotateLeft() Moves the rover left
+      */
+     public void rotateLeft() {
         dir -= 1;
-        
-        if (dir < 0)
-        {
-            dir = 3;
+        if (dir < 0) {
+            dir = 7;
         }
-        
-        System.out.println(name + " turned to the left");        
+        System.out.println(name + " rotated to the left.");        
     }
-    
+    /** 
+     * rotateRight() Moves the rover right
+     */
     public void rotateRight()
     {
         dir += 1;
-        
-        if (dir == 4)
-        {
+        if (dir == 8) {
             dir = 0;
         }
-        
-        System.out.println(name + " turned to the right");        
+        System.out.println(name + " rotated to the right.");        
     }
-
+    /**
+     * Rover is being turned in a positive or negative direction based on the int
+     * Universal Roatation
+     * Used int i, whenever it reaches 8 it'll stop the loop 
+     * After the loop, it increments 1 by i++ which stops the loop once it meets 8
+     * @param d How many times the rover should rotate 
+     * @param math.abs(d) Returns the absolute value of the int
+     */
+    public void rotate(int d) {
+        if (d >= 0) {
+            for (int i = 0; i < d; i++) {
+                rotateRight();
+            }
+        } else {
+            d = Math.abs(d);
+            for (int i = 0; i < d; i++) {
+                rotateLeft();
+            }
+        }
+    }
+    /**
+     * Returning the basic knowledge for the rover
+     */
     public String toString() 
     {
-        return "Rover[name=" + name + ", x=" + x + ", y=" + y + ", dir=" + dir + "]";
+        return "Rover[name=" + name + ", x=" + x + ", y=" + y + ", dir=" + getDirectionName(dir) + ", pics=" +numPics +"]";
+    }
+    /**
+     * 
+     */
+    public void takePicture() {
+        numPics += 1;
+        if (numPics > 5) {
+            System.out.println("Memory is full, try transferring pictures first and then try again.");
+            numPics -= 1;
+        } else {
+            System.out.println(name + "just snapped a picture at [" + x + "," + y + "] using the sepia filter!");
+        }
+    }
+    /**
+     * 
+     */
+    public void transmitPictures() {
+        System.out.println(name + " just posted " + numPics + " to Insta.");
+        numPics = 0;
+    }
+    /**
+     * 
+     */
+    public void moveTo(int x, int y) {
+        if (dir != 0) {
+            if (dir >= 0) {
+                rotate (-dir);
+            } else {
+                rotate(dir);
+            }
+        }
+        if (x >= 0) {
+            rotate(2);
+            move(x - this.x);
+            rotate(-2);
+        } else {
+            rotate(6);
+            move(Math.abs(x - this.x));
+            rotate(-6); 
+        }
+        if (y >= 0) {
+            move(y - this.y);
+        } else {
+            rotate(4);
+            move(Math.abs(y - this.y));
+            rotate(-4);
+        }
+    } 
+    /**
+     * 
+     */
+    public void goHome() {
+        moveTo(startingx, startingy);
     }
 }
+    
--- RoverRunner.java
+++ RoverRunner.java
@@ -5,30 +5,71 @@
  * @author (Carrington Jones) 
  * @version (a version number or a date)
  */
+import java.util.Scanner;
+
 public class RoverRunner
 {
-    public static void main(String[] arrrggggggs)
+    public static void main(String[] args)
     {
-        //Rover r1 = new Rover("Curiosity");
-        //Rover r2 = new Rover("Spirit");
-        //Rover r3 = new Rover ("BRUHHH");
-        //Rover r4 = new Rover("R2-D2");
+        SimpleScanner input = new SimpleScanner();
         
-        //System.out.println(r1);
+        Rover r1 = new Rover("Yoda");
+        Rover r2 = new Rover("Spirit");
+        Rover r3 = new Rover("R2-D2");
+        Rover r4 = new Rover ("BRUHHH");
         
-        //r1.move();
-        //System.out.println(r1);
+        RoverGroup group = new RoverGroup();
+        group.add(r1);
+        group.add(r2);
+        group.add(r3);
+        group.add(r4);
+        
+        boolean running = true;
+        String exitCommand = "stop";
+        
+        while (running) {
+            System.out.print("Type Rover Name Here: ");
+            String name = input.readString();
+            
+            Rover actor = group.find(name);
+            
+            if (actor != null) {
+                System.out.println("Type a menu number here:");
+                System.out.println("1. Rover movement");
+                System.out.println("2. Teleportation");
+                System.out.println("3. Rotation to the right");
+                System.out.println("4. Rotation to the left");
+                String command = input.readString();
+                if (command.equals("1")) {
+                    System.out.println("Enter x cord: ");
+                    int x = input.readInt();
+                    System.out.println("Enter y cord: ");
+                    int y = input.readInt();
 
-        //r1.rotateRight();
-        //System.out.println(r1);
-        
-        //r2.rotateLeft();
-        //System.out.println(r2);
-        
-        //r2.move();
-        //System.out.println(r2);
-        
-        //r1.move();
-        //System.out.println(r1);
+                    actor.moveTo(x, y);
+                } else if (command.equals("2")) {
+                    actor.rotateRight();
+                } else if (command.equals("3")) {
+                    actor.rotateLeft();
+                } else if (command.equals("4")) {
+                } else if (command.equals("5")) {
+                    System.out.println("Enter rover x cord: ");
+                    int x = input.readInt();
+                    System.out.println("Enter rover y cord: ");
+                    int y = input.readInt();
+
+                    actor.teleport(x, y);
+                } else {
+                    System.out.println("Failed: Command doesnt exist.");
+                }
+                System.out.println(actor);
+            } else if (name.equals(exitCommand)) {
+                running = false; 
+            } else {
+                System.out.println("Failed: Rover" + name + " could not be found.");
+            }
+            System.out.println();
+        }
+        System.out.println("Goodbye.");
     }
 }
